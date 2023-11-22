d39e6e322afee06e6eb3294ac4a81a5d5e2c3c54
Adam Kowalski <adam.kowalski.work@gmail.com>
2023-11-22 09:42:37 -0800

    formatting and python support

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,42 +1,60 @@
 return {
-  "williamboman/mason-lspconfig.nvim",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "neovim/nvim-lspconfig"
-  },
-  config = function()
-    require("mason").setup({})
-    require("mason-lspconfig").setup({
-      ensure_installed = { "rust_analyzer", "lua_ls" },
-      automatic_installation = true
-    })
-    local lspconfig = require('lspconfig')
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-      callback = function(ev)
-        local opts = { buffer = ev.buf }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'gh', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          buffer = buffer,
-          callback = function()
-            vim.cmd[[silent! lua vim.lsp.buf.format { async = false }]]
-          end
-        })
-      end,
-    })
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
-    require("lspconfig").rust_analyzer.setup({
-      capabilities = capabilities
-    })
-  end
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"neovim/nvim-lspconfig",
+		"folke/neodev.nvim",
+		"nvimtools/none-ls.nvim",
+	},
+	config = function()
+		require("neodev").setup({})
+		require("mason").setup({})
+		local servers = { "rust_analyzer", "lua_ls", "pyright" }
+		require("mason-lspconfig").setup({
+			ensure_installed = servers,
+			automatic_installation = true,
+		})
+
+		local lspconfig = require("lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+		local on_attach = function(_, bufnr)
+			local opts = { buffer = bufnr }
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+			vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts)
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+			vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+			vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
+
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = vim.api.nvim_create_augroup("LspFormatting", {}),
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ async = false })
+				end,
+			})
+		end
+
+		for _, server in ipairs(servers) do
+			lspconfig[server].setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+		end
+
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.diagnostics.codespell,
+			},
+		})
+	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   'adam-r-kowalski/nvim-treesitter',
   config = function()
     require('nvim-treesitter.configs').setup({
-      ensure_installed = { "lua", "yeti", "rust" },
+      ensure_installed = { "lua", "yeti", "rust", "python" },
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true }
